e9f1371aa98de0dd2a4b6732c2366a5e7e5ee518
25Huseyn <huseynajafov@gmail.com>
2023-03-06 19:57:57 +0400

    reloperators

---
+++ src/main/java/az/atlacademy/lesson5/reloperation.java
@@ -0,0 +1,13 @@
+package az.atlacademy.lesson5;
+
+public class reloperation {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 3;
+
+        System.out.println("a != b");
+        System.out.println("a < b");
+        System.out.println("a > b");
+        System.out.println("a == b");
+    }
+}
