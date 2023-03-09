b6527b7759e9bd1d38f098e4f3023126083def77
25Huseyn <huseynajafov@gmail.com>
2023-03-09 19:27:12 +0400

    conditions1

--- src/main/java/az/atlacademy/h1/lesson6/In3.java
+++ src/main/java/az/atlacademy/h1/lesson6/In3.java
@@ -2,7 +2,7 @@ package az.atlacademy.h1.lesson6;
 
 import java.util.Scanner;
 
-public class in3 {
+public class In3 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
