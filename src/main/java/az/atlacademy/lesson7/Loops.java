08b084f653aaac2a9c872f9b07c808f5814b7031
25Huseyn <huseynajafov@gmail.com>
2023-03-09 20:20:03 +0400

    alfabet

---
+++ src/main/java/az/atlacademy/lesson7/Loops.java
@@ -0,0 +1,8 @@
+package az.atlacademy.lesson7;
+public class Loops {
+    public static void main(String[] args) {
+        for (int i = 65; i < 91; i++) {
+            System.out.println((char) (i));
+        }
+    }
+}
