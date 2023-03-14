89e2f99d654761a417693e47963250d32a2c389c
25Huseyn <huseynajafov@gmail.com>
2023-03-14 19:40:18 +0400

    eolymp

---
+++ src/main/java/az/atlacademy/app/Test.java
@@ -0,0 +1,11 @@
+package az.atlacademy.app;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double num = scan.nextDouble();
+        System.out.printf(" %.2f", num);
+    }
+}
