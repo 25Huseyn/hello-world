d5c3cd130ea970bbb858d4ed70d704b4ab2955d2
25Huseyn <huseynajafov@gmail.com>
2023-04-17 21:31:04 +0400

    A happy family

--- src/main/java/az/atlacademy/lesson11/User.java
+++ src/main/java/az/atlacademy/lesson11/User.java
@@ -1,15 +1,15 @@
 package az.atlacademy.lesson11;
 
 public enum User {
-    deleted(3),block(6),inuse(5);
+    Deleted(3),Block(6),Inuse(5);
     private final int usercount;
     User(int usercount) {
         this.usercount = usercount;
     }
 
     public static void main(String[] args) {
-        System.out.println(deleted.usercount);
-        System.out.println(block);
-        System.out.println(inuse);
+        System.out.println(Deleted.usercount);
+        System.out.println(Block);
+        System.out.println(Inuse);
     }
 }
