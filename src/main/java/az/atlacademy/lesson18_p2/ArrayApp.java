010d630db89f5742d1f9d13c6c80d755e7ef2863
25Huseyn <huseynajafov@gmail.com>
2023-05-11 20:02:33 +0400

    Generic

---
+++ src/main/java/az/atlacademy/lesson18_p2/ArrayApp.java
@@ -0,0 +1,30 @@
+package az.atlacademy.lesson18_p2;
+
+import java.util.ArrayList;
+
+public class ArrayApp {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            nums.add((int) (Math.random() * 100));
+        }
+        System.out.println(nums);
+
+        System.out.println(nums.isEmpty());
+
+        nums.sort(Integer::compareTo);
+
+        System.out.println(nums);
+
+        nums.add(3);
+        System.out.println(nums);
+        System.out.println(nums.hashCode());
+        System.out.println(nums.remove(4));
+        nums.clear();
+        System.out.println(nums);
+    }
+}
+
+
