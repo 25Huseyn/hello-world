96337daa3f50acbd5029bbad0e9a4ce451aeca61
25Huseyn <huseynajafov@gmail.com>
2023-05-22 19:37:14 +0400

    Generic

---
+++ src/main/java/az/atlacademy/lesson21/App2.java
@@ -0,0 +1,15 @@
+package az.atlacademy.lesson21;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.Set;
+
+public class App2 {
+    public static void main(String[] args) {
+        List<Integer> evenNums = new Random().ints(20, 1, 50).filter(num -> num % 2 == 0).boxed().collect(Collectors.toList());
+        System.out.println(evenNums);
+        Set<Integer> set = evenNums.stream().filter(num -> num > 10 && num < 30).collect(Collectors.toSet());
+        System.out.println(set);
+    }
+}
