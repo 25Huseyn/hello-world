1b5821502edf8275e4528e13e4c15ba504734bba
25Huseyn <huseynajafov@gmail.com>
2023-05-25 19:36:26 +0400

    Generic

--- src/main/java/az/atlacademy/lesson22/FileApp.java
+++ src/main/java/az/atlacademy/lesson22/FileApp.java
@@ -7,13 +7,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 public class FileApp {
+    public static final String RESOURCE ="src/main/java/az/atlacademy/lesson22/RESOURCE/";
     public static void main(String[] args) {
         int[] numbers = new int[10];
         for (int i = 0; i < 10; i++) {
             numbers[i] = (int) (Math.random() * 101);
         }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("numbers.txt"))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter( RESOURCE + "numbers.txt"))) {
             for (int num : numbers) {
                 writer.write(String.valueOf(num));
                 writer.newLine();
