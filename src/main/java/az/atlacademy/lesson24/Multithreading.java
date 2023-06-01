daa0a03b577d681e2955d2c3a800eb846f579fc1
25Huseyn <huseynajafov@gmail.com>
2023-06-01 19:39:10 +0400

    commit

--- src/main/java/az/atlacademy/lesson24/Multithreading.java
+++ src/main/java/az/atlacademy/lesson24/Multithreading.java
@@ -2,12 +2,12 @@ package az.atlacademy.lesson24;
 
 public class Multithreading extends Thread {
     public static void main(String[] args) {
-        new Multithreading().run();
+            new Multithreading().run();
     }
-
+    String name = Thread.currentThread().getName();
     public void run() {
         for (int i = 0; i < 10; i++) {
-            System.out.println(new Multithreading().getClass().getName());
+            System.out.print(name);
         }
     }
 }
