b1219987beb0dd01a9cead9da9af132467b53661
25Huseyn <huseynajafov@gmail.com>
2023-07-03 19:37:30 +0400

    commit

--- src/main/java/az/atlacademy/lesson24/ThreadApp.java
+++ src/main/java/az/atlacademy/lesson24/ThreadApp.java
@@ -4,8 +4,5 @@ public class ThreadApp {
     public static void main(String[] args) {
         new Multithreading().start();
         new Multithreading().run();
-        for (int i = 0; i < 9; i++) {
-            System.out.println("Hello");
-        }
     }
 }
