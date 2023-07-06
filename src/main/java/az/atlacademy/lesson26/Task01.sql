ef356efd8ba809122f33bb764a64dbbf64cc32d6
25Huseyn <huseynajafov@gmail.com>
2023-07-06 20:03:27 +0400

    SQL

---
+++ src/main/java/az/atlacademy/lesson26/Task01.sql
@@ -0,0 +1,15 @@
+CREATE table Customers (
+CustomerId serial primary key,
+CustomerName varchar(20) NOT NULL,
+CustomerEmail varchar(50) UNIQUE,
+CustomrAge INTEGER  CHECK(age => 18),
+CustomerCity VARCHAR(35) DEFAULT 'Not İncluded';
+)
+CREATE INDEX Customers_CustomerEmail ON Customers(CustomerEmail);
+
+INSERT INTO Customers (CustomerName,CustomerEmail , CustomrAge, CustomerCity,)
+VALUES ('John ', 'john@example.com', 30, 'New York');
+INSERT INTO Customers (CustomerName,CustomerEmail , CustomrAge, CustomerCity,)
+VALUES ('Jon ', 'jon@example.com', 20, 'New York');
+INSERT INTO Customers (CustomerName,CustomerEmail , CustomrAge, CustomerCity,)
+VALUES ('Don ', 'don@example.com', 26, 'Los-Angelas');
