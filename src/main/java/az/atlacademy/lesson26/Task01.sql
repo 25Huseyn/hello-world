c8057814e4ac9da9c45e99690b99fe6c6700e9eb
25Huseyn <huseynajafov@gmail.com>
2023-07-06 20:09:11 +0400

    SQL

--- src/main/java/az/atlacademy/lesson26/Task01.sql
+++ src/main/java/az/atlacademy/lesson26/Task01.sql
@@ -2,7 +2,7 @@ CREATE table Customers (
 CustomerId serial primary key,
 CustomerName varchar(20) NOT NULL,
 CustomerEmail varchar(50) UNIQUE,
-CustomrAge INTEGER  CHECK(age => 18),
+CustomrAge INTEGER  CHECK(Customerage => 18),
 CustomerCity VARCHAR(35) DEFAULT 'Not İncluded';
 )
 CREATE INDEX Customers_CustomerEmail ON Customers(CustomerEmail);
