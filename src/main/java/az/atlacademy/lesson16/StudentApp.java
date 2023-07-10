d01f5d8e7a61f7fe6ceaf8f4fcf20a75b3412f43
25Huseyn <huseynajafov@gmail.com>
2023-07-10 12:20:49 +0400

    Comperator

--- src/main/java/az/atlacademy/lesson16/StudentApp.java
+++ src/main/java/az/atlacademy/lesson16/StudentApp.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public class StudentApp {
     public static void main(String[] args) {
-         Student[] students = {
-                new Student(3,98,"Student1"),
-                new Student(2,56,"Student2"),
-                new Student(1,56,"Student3"),
-                new Student(4,64,"Student3")
+        Student[] students = {
+                new Student(3, 98, "Student1"),
+                new Student(2, 56, "Student2"),
+                new Student(1, 56, "Student3"),
+                new Student(4, 64, "Student3")
         };
         System.out.println(Arrays.toString(students));
         Arrays.sort(students);
@@ -17,7 +17,5 @@ public class StudentApp {
         System.out.println(Arrays.toString(students));
         Arrays.sort(students, new Comperator());
         System.out.println(Arrays.toString(students));
-
     }
-
 }
