055b47345a0d1370ac26e30e7dd856d306d018c6
25Huseyn <huseynajafov@gmail.com>
2023-07-10 12:28:52 +0400

    Stream

--- src/main/java/az/atlacademy/lesson21/StreamApp2.java
+++ src/main/java/az/atlacademy/lesson21/StreamApp2.java
@@ -3,6 +3,7 @@ package az.atlacademy.lesson21;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+
 public class StreamApp2 {
     public static void main(String[] args) {
         /*Collection<String> strings = new ArrayList<>(Arrays.asList("Apple", "Banana", "Orange"));
