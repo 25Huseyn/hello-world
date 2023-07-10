7ed669f158f6a80604162109e137b5d54c8a7ca5
25Huseyn <huseynajafov@gmail.com>
2023-07-10 12:27:14 +0400

    Stacks

--- src/main/java/az/atlacademy/lesson20/OrderApp.java
+++ src/main/java/az/atlacademy/lesson20/OrderApp.java
@@ -7,17 +7,20 @@ import java.util.UUID;
 
 public class OrderApp {
     private static final Queue<Order<String>> queue = new LinkedList<>();
+
     public static void main(String[] args) {
 
     }
-    public static String makerOrder(){
+
+    public static String makerOrder() {
         final String id = UUID.randomUUID().toString();
         queue.add(new Order<>(id));
         return id;
     }
-    public static Optional<String> processOrder(){
+
+    public static Optional<String> processOrder() {
         final Order<String> order = queue.peek();
-        if (order == null){
+        if (order == null) {
             return Optional.empty();
         }
         queue.poll();
--- src/main/java/az/atlacademy/lesson20/StackApp.java
+++ src/main/java/az/atlacademy/lesson20/StackApp.java
@@ -5,9 +5,11 @@ import java.util.LinkedList;
 
 public class StackApp {
     private static final Deque<operation> stack = new LinkedList<>();
+
     public static void main(String[] args) {
         doOperation();
     }
+
     public static String doOperation() {
         int random = (int) (Math.random() * operation.values().length);
         stack.add(operation.values()[random]);
