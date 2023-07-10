e91d0344937ba4cc03fd83be04a53cf80f4f5345
25Huseyn <huseynajafov@gmail.com>
2023-07-10 12:21:38 +0400

    Comparator

--- src/main/java/az/atlacademy/lesson16_p2/PlayerApp.java
+++ src/main/java/az/atlacademy/lesson16_p2/PlayerApp.java
@@ -5,10 +5,10 @@ import java.util.Comparator;
 
 public class PlayerApp {
     public static void main(String[] args) {
-         Player[] players = {
-                new Player(20,"Smith"),
-                new Player(15,"Jones"),
-                new Player(20,"Jones")
+        Player[] players = {
+                new Player(20, "Smith"),
+                new Player(15, "Jones"),
+                new Player(20, "Jones")
         };
         System.out.println(Arrays.toString(players));
         Arrays.sort(players);
@@ -16,24 +16,24 @@ public class PlayerApp {
 
         System.out.println("----------- ");
         System.out.println(Arrays.toString(players));
-        Arrays.sort(players,new PlayerScoreDescComparator());
+        Arrays.sort(players, new PlayerScoreDescComparator());
         System.out.println(Arrays.toString(players));
 
         System.out.println("----------- ");
         System.out.println(Arrays.toString(players));
-        Arrays.sort(players,new PlayerNameDescComparator());
+        Arrays.sort(players, new PlayerNameDescComparator());
         System.out.println(Arrays.toString(players));
 
         System.out.println("----------- ");
         System.out.println(Arrays.toString(players));
-        Arrays.sort(players, (player1, player2) -> Integer.compare(player1.getScore(),player2.getScore()));
+        Arrays.sort(players, (player1, player2) -> Integer.compare(player1.getScore(), player2.getScore()));
         System.out.println(Arrays.toString(players));
 
 
         for (int i = 0; i < players.length; i++) {
             System.out.println(players[i]);
         }
-        for (Player player : players ){
+        for (Player player : players) {
             System.out.println(player);
         }
     }
