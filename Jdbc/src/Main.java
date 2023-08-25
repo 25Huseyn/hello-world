1a953af0f9b9640a72aacd3d9cba60a6c3bbdac7
25Huseyn <huseynajafov@gmail.com>
2023-08-25 12:14:58 +0400

    JDBC

--- Jdbc/src/Main.java
+++ Jdbc/src/Main.java
@@ -4,13 +4,13 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, SQLException {
-       Class postgresDriverClass = Class.forName("org.postgresql.Driver");
+        Class postgresDriverClass = Class.forName("org.postgresql.Driver");
         Constructor postgresDriverClassConstructor = postgresDriverClass.getDeclaredConstructor();
         Driver postgresDriverObject = (Driver) postgresDriverClassConstructor.newInstance();
 
         DriverManager.registerDriver(postgresDriverObject);
 
-       Connection dbConnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/lesson28db_homework","postgres","mysecretpassword");
+        Connection dbConnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/lesson28db_homework", "postgres", "mysecretpassword");
         Statement statement = dbConnection.createStatement();
 
         ResultSet resultSet = statement.executeQuery("SELECT FirstName, Name, OrderDetails.Quantity, Products.Price, OrderDetails.Quantity * Products.Price as totalPrice\n" +
@@ -20,7 +20,7 @@ public class Main {
                 "         JOIN Customers ON Customers.CustomerID = Orders.CustomerID\n" +
                 "Where Customers.FirstName = 'Jane';");
 
-        while (resultSet.next()){
+        while (resultSet.next()) {
             String FirstName = resultSet.getString("FirstName");
             String Name = resultSet.getString("Name");
             double Price = resultSet.getDouble("Price");
