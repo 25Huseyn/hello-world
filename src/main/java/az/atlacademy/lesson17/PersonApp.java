d8365c56349bb8336837d9270d4e35de00678138
25Huseyn <huseynajafov@gmail.com>
2023-08-25 12:53:01 +0400

    PersonComperator

--- src/main/java/az/atlacademy/lesson17/PersonApp.java
+++ src/main/java/az/atlacademy/lesson17/PersonApp.java
@@ -8,12 +8,7 @@ import java.util.Arrays;
 
 public class PersonApp {
     public static void main(String[] args) {
-        Person[] persons = {
-                new Person(1, "Person4", 23, 56.4),
-                new Person(2, "Person1", 23, 56.4),
-                new Person(3, "Person3", 23, 56.4),
-                new Person(4, "Person2", 45, 67.5)
-        };
+        Person[] persons = {new Person(1, "Person4", 23, 56.4), new Person(2, "Person1", 23, 56.4), new Person(3, "Person3", 23, 56.4), new Person(4, "Person2", 45, 67.5)};
         System.out.println(Arrays.toString(persons));
         Arrays.sort(persons);
         System.out.println(Arrays.toString(persons));
