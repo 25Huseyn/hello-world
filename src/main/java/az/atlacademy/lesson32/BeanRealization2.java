a65157f47e51eec2dadd9df9ae6b553c46c650bc
25Huseyn <huseynajafov@gmail.com>
2023-08-25 13:02:53 +0400

    Bean

--- src/main/java/az/atlacademy/lesson32/BeanRealization2.java
+++ src/main/java/az/atlacademy/lesson32/BeanRealization2.java
@@ -1,6 +1,6 @@
 package az.atlacademy.lesson32;
 
-public class BeanRealization2 implements IBeanInterfeace{
+public class BeanRealization2 implements IBeanInterfeace {
     @Override
     public void printsomething() {
         System.out.println("This is Bean 2");
