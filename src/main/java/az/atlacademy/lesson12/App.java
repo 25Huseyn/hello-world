f8f452b28c4a34c045ce877160f467a596d3ed05
25Huseyn <huseynajafov@gmail.com>
2023-08-25 12:49:48 +0400

    App

--- src/main/java/az/atlacademy/lesson12/App.java
+++ src/main/java/az/atlacademy/lesson12/App.java
@@ -9,13 +9,12 @@ public class App {
         String word1 = scan.next();
         int num = 0;
         int num1 = 0;
-           try {
-                num = Integer.parseInt(word);
-               num1 = Integer.parseInt(word1);
-               System.out.println(num / num1);
-           }
-           catch (NumberFormatException | ArithmeticException e){
-               System.out.println(e);
-           }
+        try {
+            num = Integer.parseInt(word);
+            num1 = Integer.parseInt(word1);
+            System.out.println(num / num1);
+        } catch (NumberFormatException | ArithmeticException e) {
+            System.out.println(e);
+        }
     }
 }
